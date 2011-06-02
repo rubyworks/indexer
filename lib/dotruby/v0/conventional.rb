81d65884b03cfdab52f2997b2ca952b9c5c703cb
7rans <transfire@gmail.com>
2011-06-02 00:11:18 -0400

    plenty of imporvements to Conventional module

--- lib/dotruby/v0/conventional.rb
+++ lib/dotruby/v0/conventional.rb
@@ -58,6 +58,21 @@ module DotRuby
       end
 
       #
+      # Sets the creation date of the project.
+      #
+      # @param [String] date
+      #   The creation date of this project.
+      #
+      def created=(date)
+        @date = case date
+                when String
+                  Date.parse(date)
+                else
+                  date
+                end
+      end
+
+      #
       # The primary license of the project.
       #
       # @return [String, nil]
@@ -73,9 +88,13 @@ module DotRuby
       # @param [Array, String] license
       #   The license(s) of the project.
       #
-      def license=(licenses)
+      def licenses=(licenses)
         @licenses = [licenses].flatten
       end
+
+      #
+      # Singular alias for #licenses=.
+      alias :licence= :licenses=
 
       #
       # Sets the authors of the project.
@@ -99,7 +118,9 @@ module DotRuby
       # @param [Array<String>, String] maintainer
       #   The current maintainers of the project.
       #
+      #--
       # TODO: if an entry is just a string convert it to a hash.
+      #++
       def maintainers=(maintainers)
         @maintainers = case manitainers
                        when Array
@@ -110,7 +131,8 @@ module DotRuby
       end
 
       #
-      # The primary email address of the project.
+      # The primary email address of the project. The email address
+      # is taken from the first listed maintainer.
       #
       # @return [String, nil]
       #   The primary email address for the project.
@@ -148,136 +170,168 @@ module DotRuby
       end
 
       #
+      # Sets the packages with which this package is known to have
+      # incompatibilites.
       #
+      # @param [Array, String] replacements
+      #   The conflicts for the project.
       #
       def conflicts=(conflicts)
-        @replaces = case conflicts
-                    when Array
-                      conflicts
-                    else
-                      [conflicts]
-                    end
+        unless conflicts.kind_of?(Hash)
+          raise(InvalidMetadata, "conflicts must be a Hash")
+        end
+
+        @conflicts.clear
+
+        conflicts.each do |name, specifics|
+          add_conflict(name, specifics)
+        end
       end
 
       #
-      #
+      # Sets the packages this package could (more or less) replace.
       # 
-      def replaces=(replaces)
-        @replaces = case replaces
-                    when Array
-                      replaces
-                    else
-                      [replaces]
-                    end
+      # @param [Array, String] replacements
+      #   The replacements for the project.
+      #
+      def replacements=(replacements)
+        unless replacements.kind_of?(Hash)
+          raise(InvalidMetadata, "replacements must be a Hash")
+        end
+
+        @replacements.clear
+
+        replacements.each do |name, specifics|
+          add_replacement(name, specifics)
+        end
       end
 
       #
-      # Sets the external requirements of the project.
+      # Alternate short name for #replacements.
       #
-      # @param [Array, String] requirements
-      #   The external requirements.
+      alias :replaces= :replacements=
+
+      #
+      # Sets the requirements of the project. Also commonly refered
+      # to as dependencies.
+      #
+      # @param [Hash{String => String}] requirements
+      #   The dependency names and specifics.
+      #
+      # @raise [InvalidMetadata]
+      #   The requirements must be a `Hash`.
       #
       def requirements=(requirements)
-        @requirements = case requirements
-                        when Array
-                          requirements
-                        else
-                          [requirements]
-                        end
+        unless requirements.kind_of?(Hash)
+          raise(InvalidMetadata, "requirements must be a Hash")
+        end
+
+        @requirements.clear
+
+        requirements.each do |name, specifics|
+          add_requirement(name, specifics)
+        end
       end
 
+      #
+      # Alternate short name for #requirements.
       #
       alias :requires= :requirements=
 
       #
-      # Sets the dependencies of the project.
+      # Common alias for #requirements.
       #
-      # @param [Hash{String => String}] dependencies
-      #   The dependency names and versions listed in the metadata file.
-      #
-      # @raise [InvalidMetadata]
-      #   The dependencies must be a `Hash`.
-      #
-      def dependencies=(dependencies)
-        unless dependencies.kind_of?(Hash)
-          raise(InvalidMetadata, "dependencies must be a Hash")
-        end
-
-        @dependencies.clear
-
-        dependencies.each do |name,versions|
-          add_dependency(name,versions)
-        end
-      end
+      alias :dependencies= :requirements=
 
       #
-      # Adds a new runtime dependency.
+      # Adds a new requirement.
       #
       # @param [String] name
-      #   The name of the dependency.
+      #   The name of the requirement.
       #
-      # @param [Array<String>, String] versions
-      #   The required versions of the dependency.
+      # @param [Hash] specifics
+      #   The specifics of the requirement.
       #
-      def add_runtime_dependency(name,versions)
-        @runtime_dependencies << Dependency.parse(name,versions)
+      def add_requirement(name, specifics)
+        @requirements << Requirement.parse(name, specifics)
       end
 
       #
-      # Sets the runtime-dependencies of the project.
-      #
-      # @param [Hash{String => String}] dependencies
-      #   The runtime-dependency names and versions listed in the metadata
-      #   file.
-      #
-      # @raise [InvalidMetadata]
-      #   The runtime-dependencies must be a `Hash`.
-      #
-      def runtime_dependencies=(dependencies)
-        unless dependencies.kind_of?(Hash)
-          raise(InvalidMetadata,"runtime_dependencies must be a Hash")
-        end
-
-        @runtime_dependencies.clear
-
-        dependencies.each do |name,versions|
-          add_runtime_dependency(name,version)
-        end
-      end
-
-      #
-      # Adds a new development dependency.
+      # Adds a new replacement.
       #
       # @param [String] name
-      #   The name of the dependency.
+      #   The name of the replacement.
       #
-      # @param [Array<String>, String] versions
-      #   The required versions of the dependency.
+      # @param [Hash] specifics
+      #   The specifics of the replacement.
       #
-      def add_development_dependency(name,versions)
-        @development_dependencies << Dependency.parse(name,versions)
+      def add_replacement(name, specifics)
+        @replacements << Requirement.parse(name, specifics)
       end
 
       #
-      # Sets the development-dependencies of the project.
+      # Adds a new conflict.
       #
-      # @param [Hash{String => String}] dependencies
-      #   The development-dependency names and versions listed in the
-      #   metadata file.
+      # @param [String] name
+      #   The name of the conflict package.
       #
-      # @raise [InvalidMetadata]
-      #   The development-dependencies must be a `Hash`.
+      # @param [Hash] specifics
+      #   The specifics of the conflict package.
       #
-      def development_dependencies=(dependencies)
-        unless dependencies.kind_of?(Hash)
-          raise(InvalidMetadata,"development_dependencies must be a Hash")
-        end
+      def add_conflict(name, specifics)
+        @conflicts << Requirement.parse(name, specifics)
+      end
 
-        @development_dependencies.clear
-
-        dependencies.each do |name,versions|
-          add_development_dependency(name,version)
+      #
+      # Returns the runtime requirements of the project.
+      #
+      # @return [Array<Requirement>] runtime requirements.
+      #
+      def runtime_requirements
+        requirements.select do |requirement|
+          requirements.runtime?
         end
+      end
+
+      #
+      #
+      #
+      alias :runtime_dependencies, :runtime_requirements
+
+      #
+      # Returns the development requirements of the project.
+      #
+      # @returns [Array<Requirement>] development requirements.
+      #
+      def development_requirements
+        requirements.select do |requirement|
+          ! requirements.runtime?
+        end
+      end
+
+      #
+      #
+      #
+      alias :development_dependencies, :development_requirements
+
+      #
+      # Sets the external requirements for the project.
+      #
+      # External requirements are non-package requirements refering
+      # instead to arbitrary resources. These might be as specific as
+      # a system library such as "libXML2 1.0+", or as generic as a
+      # "fast graphics card".
+      #
+      # @param [Array, String] requirements
+      #   The external requirements for the prokect.
+      #
+      def exteneral_requirements=(requirements)
+        @external_requirements = case requirements
+                                 when Array
+                                   requirements
+                                 else
+                                   [requirements]
+                                 end
       end
 
 =begin
@@ -293,7 +347,7 @@ module DotRuby
 =end
 
       #
-      # Extra user-defined metdata.
+      # Set extraneous user-defined metdata.
       #
       def extra=(extra)
         unless extra.kind_of?(Hash)
@@ -305,7 +359,33 @@ module DotRuby
       #
       # Convert convenience form of metadata to canonical form.
       #
+      #--
+      # TODO: Maybe this code should be a in different file?
+      #++
       def to_canonical
+        data = {}
+        instance_variables.each do |iv|
+          name = iv.to_s.sub(/^\@/, '')
+          data[name] = send(name)
+        end
+
+        data['date']    = date.strftime('%Y-%m-%d') if date
+
+        data['created'] = date.strftime('%Y-%m-%d') if created
+
+        data['requirements'] = requirements.inject({}) do |h, (k,v)|
+          h[k] = v.to_h
+        end
+
+        data['replacements'] = replacements.inject({}) do |h, (k,v)|
+          h[k] = v.to_h
+        end
+
+        data['conflicts'] = conflicts.inject({}) do |h, (k,v)|
+          h[k] = v.to_h
+        end
+
+        CanonicalMetadata.new(data)
       end
 
     protected
@@ -314,16 +394,16 @@ module DotRuby
       # Initializes the {Metdata} attributes.
       #
       def initialize_attributes
-        @licenses    = []
-        @authors     = []
-        @maintatiner = []
+        @licenses              = []
+        @authors               = []
+        @maintatiners          = []
 
-        @require_paths = []
+        @require_paths         = []
 
-        @requirements = []
-        @dependencies = []
-        @runtime_dependencies = []
-        @development_dependencies = []
+        @requirements          = []
+        @replacements          = []
+        @conflicts             = []
+        @external_requirements = []
 
         #@files = []
       end
