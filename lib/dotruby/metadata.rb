fb9105f57f6fe019be1f039a5fce6c530a390aef
Postmodern <postmodern.mod3@gmail.com>
2011-06-03 17:12:27 -0700

    Only consume the 'revision' key. We should only consume Strings.

--- lib/dotruby/metadata.rb
+++ lib/dotruby/metadata.rb
@@ -6,9 +6,7 @@ module DotRuby
 
     #
     def initialize(data={})
-      @revision = data['revision'] ||
-                  data[:revision]  ||
-                  CURRENT_REVISION
+      @revision = (data['revision'] || CURRENT_REVISION)
 
       extend DotRuby.v(revision)::Attributes
       extend DotRuby.v(revision)::Conventional
