d17a70f47f434156e4606f4dfc724dbdf3330a97
Postmodern <postmodern.mod3@gmail.com>
2011-06-03 17:13:49 -0700

    Delete the 'revision' key from the data, before it is passed to merge!.

--- lib/dotruby/metadata.rb
+++ lib/dotruby/metadata.rb
@@ -6,7 +6,7 @@ module DotRuby
 
     #
     def initialize(data={})
-      @revision = (data['revision'] || CURRENT_REVISION)
+      @revision = (data.delete('revision') || CURRENT_REVISION)
 
       extend DotRuby.v(revision)::Attributes
       extend DotRuby.v(revision)::Conventional
