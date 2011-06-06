73e84b350f60eeaef0bc9470bdbf12ea1b76a7d1
7rans <transfire@gmail.com>
2011-06-05 20:13:05 -0400

    Spec can't be a subclass, but rather delegates io functions

--- lib/dotruby/spec.rb
+++ lib/dotruby/spec.rb
@@ -2,31 +2,43 @@ require 'dotruby/metadata'
 
 module DotRuby
 
-  # Specific Specification.
+  # Spec the specification generalized for the convenience of developers.
+  # It offers method aliases and models various parts of the specification 
+  # with useful classes.
   #
-  class Spec < Metadata
-    FILE_NAME = '.rbuy'
-
+  class Spec #< Metadata
     include HashLike
+
+    #
+    FILE_NAME = '.ruby'
+
+    #
+    def initialize(data={})
+      @revision = data.delete('revision') || CURRENT_REVISION
+
+      extend DotRuby.v(@revision)::Attributes
+      extend DotRuby.v(@revision)::Conventional  # not Canonical
+
+      initialize_attributes
+      merge!(data)
+    end
 
     # Save `.ruby` file.
     #
     # @param [String] file
     #   The file name in which to save the metadata as YAML.
     #
-    def save!(file=FILE_NAME)
-      File.open(file, 'w') do |f|
-        to_yaml(f)
-      end
+    def save!(file='.ruby')
+      to_metadata.save!(file)
     end
 
     # Read `.ruby` from file.
     #
     # @param [String] file
-    #   The file name in which to read the YAML metadata.
+    #   The file name from which to read the YAML metadata.
     #
     def self.read(file)
-      new(YAML.load_file(file))
+      Metadata.read(file).to_spec
     end
 
     # Find project root and read `.ruby` file.
@@ -34,8 +46,8 @@ module DotRuby
     # @param [String] from
     #   The directory from which to start the upward search.
     #
-    def self.find(from=Dir.pwd)
-      read(File.join(root(from),FILE_NAME))
+    def self.find(dir=Dir.pwd)
+      Metadata.find(dir).to_spec
     end
 
     # Find project root by looking upward for a `.ruby` file.
@@ -50,13 +62,7 @@ module DotRuby
     #   The `.ruby` file could not be located.
     #
     def self.root(from=Dir.pwd)
-      Dir.ascend(from) do |path|
-        if File.file?(File.join(path,FILE_NAME))
-          return path
-        end
-      end
-
-      raise("could not locate the #{FILE_NAME} file")
+      Metadata.root(dir)
     end
 
   end
