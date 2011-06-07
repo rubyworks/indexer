65d2ed58b461c0b0a848fab3af6e255fac415d69
7rans <transfire@gmail.com>
2011-06-07 12:32:10 -0400

    implement simple cli dispatcher

--- lib/dotruby/cli.rb
+++ lib/dotruby/cli.rb
@@ -1,7 +1,19 @@
 module DotRuby
 
-  # Execute command line tool.
+  # Command line interface.
+  #
+  # To write a commnd for DotRuby simply create an executable script
+  # with the name `dotruby-{name}`.
+  #
   def self.cli(*argv)
+    command = argv.shift
+    if command
+      cmd = "dotruby-#{command} " + argv.join(' ') 
+      system cmd
+    else
+      puts "DotRuby v#{VERSION}"
+      puts COPYRIGHT
+    end
   end
 
 end
