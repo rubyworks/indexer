64f564a77ccba8f4daf0eb30fc889a1b3c6103e4
7rans <transfire@gmail.com>
2011-07-09 16:01:24 -0400

    Add Copyright class.

---
+++ lib/dotruby/v0/copyright.rb
@@ -0,0 +1,89 @@
+module DotRuby
+
+  # Copyright class models ...
+  class Copyright
+
+    # Parse copyright entry.
+    #
+    def self.parse(copyright)
+      case copyright
+      when Array
+        year, holder, license = *copyright
+      when Hash
+        year    = copyright['year']    || copyright[:year]
+        holder  = copyright['holder']  || copyright[:holder]
+        license = copyright['license'] || copyright[:license]
+      when String
+        c = copyright.dup
+        c = c.sub(/(copr\.|copyright)/i,'').sub(/\(c\)/i,'').strip
+        if /^(.*?\d\d\d\d)/ =~ c
+          year = $1
+          c = c.sub(year.to_s,'').strip
+        end
+        if /(\(.*?\))/ =~ c
+          license = $1[1..-2]
+          c = c.sub($1.to_s,'').strip
+        end
+        holder = c
+      else
+        raise ValidationError, "copyright"
+      end
+      new(holder, year, license)
+    end
+
+    #
+    def initialize(holder, year=nil, license=nil)
+      self.holder  = holder
+      self.year    = year    if year
+      self.license = license if license
+    end
+
+    #
+    attr :year
+
+    #
+    attr :holder
+
+    #
+    attr :license
+
+    #
+    def year=(year)
+      Valid.copyright_year!(year, "copyright.year")
+      @year = year
+    end
+
+    #
+    def holder=(holder)
+      Valid.oneline!(holder, "copyright.holder")
+      @holder = holder
+    end
+
+    #
+    def license=(license)
+      Valid.oneline!(license, "copyright.license")
+      @license = license
+    end
+
+    # Standard copyright stamp.
+    #
+    def to_s
+      s = ["Copyright (c)"]
+      s << year if year
+      s << holder
+      s << "(#{lecense})" if license
+      s.join(' ') + ". All Rights Reserved."
+    end
+
+    #
+    def to_h
+      h = {}
+      h['holder']  = holder
+      h['year']    = year    if year
+      h['license'] = license if license
+      h
+    end
+
+  end
+
+end
--- qed/spec/23_copyrights.md
+++ qed/spec/23_copyrights.md
@@ -28,6 +28,33 @@ Or as a single string, for which there is a singular alias.
 
     spec.copyright = "Copyright (c) 2010 T. Bone Willy"
 
+It can also be given as an array of three element arrays.
+
+    spec.copyrights = [
+      ["2010", "T. Bone Willy", "BSD-2-Clause"]
+    ]
+
+    spec.copyrights[0].year.should    == "2010"
+    spec.copyrights[0].holder.should  == "T. Bone Willy"
+    spec.copyrights[0].license.should == "BSD-2-Clause"
+
+The Array entry form can be used as a single assigment
+on the singular method.
+
+    spec.copyright = ["2010", "T. Bone Willy", "BSD-2-Clause"]
+
+    spec.copyrights[0].year.should    == "2010"
+    spec.copyrights[0].holder.should  == "T. Bone Willy"
+    spec.copyrights[0].license.should == "BSD-2-Clause"
+
+But not the plural.
+
+    expect DotRuby::ValidationError do
+      spec.copyrights = ["2010", "T. Bone Willy", "BSD-2-Clause"]
+    end
+
+NOTE: Maybe in the future a more intelligent parser could handle this.
+
 But it can't be any other value.
 
     no 100
