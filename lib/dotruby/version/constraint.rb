a541cff54614bf23538cfe18e61af2c2d361b383
7rans <transfire@gmail.com>
2011-07-09 13:52:25 -0400

    Improve Constraint class.

--- lib/dotruby/version/constraint.rb
+++ lib/dotruby/version/constraint.rb
@@ -2,6 +2,8 @@ module DotRuby
 
   module Version
 
+    # The Constraint class models a single version equality or inequality.
+    # It consists of the operator and the version number.
     #--
     # TODO: Please improve me!
     #
@@ -11,6 +13,28 @@ module DotRuby
     #++
     class Constraint
 
+      #
+      def self.parse(constraint)
+        new(constraint)
+      end
+
+      #
+      def self.[](operator, number)
+        new([operator, number])
+      end
+
+      #
+      def initialize(constraint)
+        @operator, @number = parse(constraint || '0+')
+
+        case constraint
+        when Array
+          @stamp = "%s %s" % [@operator, @number]
+        when String
+          @stamp = constraint || '0+'
+        end
+      end
+
       # Verison number.
       attr :number
 
@@ -18,36 +42,55 @@ module DotRuby
       attr :operator
 
       #
-      def initialize(constraint)
-        @constraint = constraint || '0+'
-        @operator, @number = *parse(@constraint)
+      def to_s
+        @stamp
       end
 
-      # Converts the version into a constraint recognizable by RubyGems.
-      def constraint
-        @constraint
+      # Converts the version into a constraint string recognizable
+      # by RubyGems.
+      #--
+      # TODO: Better name Constraint#to_s2.
+      #++
+      def to_gem_version
+        op = '~>' if operator == '=~'
+        "%s %s" % [operator, number]
       end
+
+      #
+      def to_proc
+        lambda do |t|
+          n = Version::Number.parse(t)
+          n.send(operator, number)
+        end
+      end
+
+    private
 
       #
       def parse(constraint)
         case constraint
+        when Array
+          op, num = constraint
         when /^(.*?)\~$/
-          ["~>", $1]
+          op, val = "=~", $1
         when /^(.*?)\+$/
-          [">=", $1]
+          op, val = ">=", $1
         when /^(.*?)\-$/
-          ["<", $1]
+          op, val = "<", $1
+        when /^(=~|~>|<=|>=|==|=|<|>)?\s*(\d+(:?[-.]\w+)*)$/
+          if op = $1
+            op = '=~' if op == '~>'
+            op = '==' if op == '='
+            val = $2.split(/\W+/)
+          else
+            op = '=='
+            val = constraint.split(/\W+/)
+          end
         else
-          constraint.split(/\s+/)
+          raise ArgumentError #constraint.split(/\s+/)
         end
+        return op, Version::Number.new(val)
       end
-
-      #
-      def to_s
-        "#{constraint}"
-      end
-
-    private
 
       # Parse package entry into name and version constraint.
       #def parse(package)
