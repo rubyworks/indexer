b6b97ecbd9e78a3a803ec90d1e09d2b28b5b47c0
7rans <transfire@gmail.com>
2011-07-10 22:58:57 -0400

    Add #v method to Model.

--- lib/dotruby/model.rb
+++ lib/dotruby/model.rb
@@ -12,14 +12,16 @@ module DotRuby
       if not DotRuby.const_defined?(basename)
         mod = Module.new
         mod.module_eval %{
+          def self.v(revision, *args, &blk)
+            V[revison]::#{basename}.new(*args, &blk)
+          end
           def self.new(*args, &blk)
             V[CURRENT_REVISION]::#{basename}.new(*args, &blk)
           end
         }
         DotRuby.const_set(basename, mod)
       else
-        c = DotRuby::const_get(basename)
-        # if version is greater than presently defined
+        DotRuby.const_get(basename)
       end
     end
 
