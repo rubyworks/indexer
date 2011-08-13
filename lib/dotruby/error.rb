4ddfb92bdee88602d70c7a8605d159392f989dbc
7rans <transfire@gmail.com>
2011-08-12 20:10:01 -0400

    Improve Error tag module.

--- lib/dotruby/error.rb
+++ lib/dotruby/error.rb
@@ -2,10 +2,17 @@ module DotRuby
   # Tag module for DotRuby Exceptions.
   #
   # Use this module to extend arbitrary errors raised by DotRuby,
-  # so they can be easily identified as DotRuby errors if needs be. 
+  # so they can be easily identified as DotRuby errors if need be. 
   module Error
     # Just catch the error and raise this instead.
     def self.exception(msg=nil,orig=$!)
+      if Class === orig
+        orig = orig.new(msg)
+      elsif orig.nil?
+        orig = StandardError.new(msg)
+      else
+        orig.message = msg if msg
+      end
       orig.extend self
       orig
     end
