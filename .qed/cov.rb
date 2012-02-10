ab39d6b6efd7035ed99fd37b5d9eed03c00b0fbf
7rans <transfire@gmail.com>
2012-02-10 10:16:10 -0500

    QED now uses confile and needs ae loaded explicitly. [admin]

--- .qed/cov.rb
+++
@@ -1,9 +0,0 @@
-# TODO: ultimately we can break this down into groups based on version (vo, v1 ext)
-require 'simplecov'
-SimpleCov.start do
-  coverage_dir 'log/coverage'
-  add_group "Shared" do |src_file|
-    /lib\/dotruby\/v(\d+)(.*?)$/ !~ src_file.filename
-  end
-  add_group "Revision 0", "lib/dotruby/v0"
-end
