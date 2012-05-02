ec1f857910ce23a1e6d27dc71c9e68d6eef3c47e
7rans <transfire@gmail.com>
2012-05-02 17:50:45 -0400

    Put QED config in separate file. [admin]

--- config/qed-simplecov.rb
+++ config/qed-simplecov.rb
@@ -1,13 +1,9 @@
-#!/usr/bin/env ruby
-
-profile :cov do
-  require 'simplecov'
-  SimpleCov.start do
-    coverage_dir 'log/coverage'
-    add_group "Shared" do |src_file|
-      /lib\/dotruby\/v(\d+)(.*?)$/ !~ src_file.filename
-    end
-    add_group "Revision 0", "lib/dotruby/v0"
+require 'simplecov'
+SimpleCov.start do
+  coverage_dir 'log/coverage'
+  add_group "Shared" do |src_file|
+    /lib\/dotruby\/v(\d+)(.*?)$/ !~ src_file.filename
   end
+  add_group "Revision 0", "lib/dotruby/v0"
 end
 
