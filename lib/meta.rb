92096e1c7536fa2eb69bdd00b880d837d2443962
7rans <transfire@gmail.com>
2012-08-04 13:29:01 -0400

    Name will be simple 'meta' after all.

--- lib/meta.rb
+++ lib/meta.rb
@@ -7,21 +7,15 @@ require 'metadata/error'
 require 'metadata/valid'
 
 require 'metadata/model'
-#require 'metadata/factories'
-
-require 'metadata/spec'
 
 require 'metadata/version/exceptions'
 require 'metadata/version/number'
 require 'metadata/version/constraint'
 
 require 'metadata/builder'
-
-# If we ever descide to support plugins in the future.
-#require 'metaspec/plugins'
-#Metadata.load_plugins
-
 #require 'metadata/bundler'
 
 require 'metadata/v0' # CURRENT_REVISION
+
+require 'metadata/metadata'
 
