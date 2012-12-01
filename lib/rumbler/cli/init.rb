c92ff115f7c5e6e37663f3fa4a2d9e8db7fd45f5
7rans <transfire@gmail.com>
2012-12-01 08:39:16 -0500

    Add ERB templating to init command.

--- lib/rumbler/cli/init.rb
+++ lib/rumbler/cli/init.rb
@@ -8,46 +8,64 @@ module Rumbler
     def run_init(*argv)
       yaml = false
 
-      begin
-        parser = OptionParser.new do |opts|
-          opts.banner = "Usage: #{$0} init"
-          opts.on_tail('--yaml', 'use a YAML-based Metafile') do
-            yaml = true
-          end
-          opts.on_tail('--debug', 'run with $DEBUG set to true') do
-            $DEBUG = true
-          end
-          opts.on_tail('-h', '--help', 'read this help message') do
-            puts opts
-            exit
-          end
+      require 'erb'
+
+      parser = OptionParser.new do |opts|
+        opts.banner = "Usage: #{$0} init"
+        opts.on_tail('--yaml', 'use a YAML-based Metafile') do
+          yaml = true
         end
-
-        parser.parse!(argv)
-
-        template_dir = File.join(DATADIR, "v#{REVISION}")
-
-        if yaml
-          template = File.join(template_dir, 'yaml.txt')
-        else
-          template = File.join(template_dir, 'ruby.txt')
+        opts.on_tail('--debug', 'run with $DEBUG set to true') do
+          $DEBUG = true
         end
-
-        if File.exists?('Metadata')
-          raise Error.exception("Metadata file already exists.", IOError) 
-        else
-          File.open('Metadata', 'w') do |f|
-            f << File.read(template)
-          end
-        end
-      rescue => error
-        if $DEBUG
-          raise error
-        else
-          $stderr.puts "#{error}"
-          exit -1
+        opts.on_tail('-h', '--help', 'read this help message') do
+          puts opts
+          exit
         end
       end
+
+      parser.parse!(argv)
+
+      if File.exists?('Metadata')
+        raise Error.exception("Metadata file already exists.", IOError) 
+      end
+
+      template_dir = File.join(DATADIR, "v#{REVISION}")
+
+      if yaml
+        template_file = File.join(template_dir, 'yaml.txt')
+      else
+        template_file = File.join(template_dir, 'ruby.txt')
+      end
+
+      metadata = Metadata.new
+
+      if gemspec = find_gemspec
+        metadata.import_gemspec(gemspec)
+      end
+
+      template = ERB.new(File.read(template_file))
+      result   = template.result(Form.new(metadata).binding)
+
+      File.open('Metadata', 'w') do |f|
+        f << result
+      end
+    end
+
+    #
+    def find_gemspec
+      Dir['{,pkg/}*.gemspec'].first
+    end
+
+    #
+    class Form
+      def initialize(metadata)
+        @metadata = metadata
+      end
+      def method_missing(s, *a, &b)
+        @metadata.public_send(s, *a, &b) || '<fill-out #{s}>'
+      end
+      public :binding
     end
 
   end
